2ed70546549f650e5f6ecac401678ad77207294e
javayouna <106093094+javayouna@users.noreply.github.com>
2022-10-26 23:14:48 +0900

    1026

--- src/main/java/com/mysite/sbb/entity/Question.java
+++ src/main/java/com/mysite/sbb/entity/Question.java
@@ -1,10 +1,7 @@
 package com.mysite.sbb.entity;
 
-
-<<<<<<< HEAD
-=======
 import lombok.Data;
->>>>>>> 412e084 (Initial commit)
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,10 +12,8 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-<<<<<<< HEAD
-=======
 @Data
->>>>>>> 412e084 (Initial commit)
+
 public class Question {
     @Id //기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY) //자동1씩 증가
